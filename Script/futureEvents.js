11f649cc2ace35986b50008c8de2576aefd3f190
Federeye5 <federeyes2000@gmail.com>
2023-03-02 17:17:48 -0300

    retoquesJS

--- Script/futureEvents.js
+++ Script/futureEvents.js
@@ -1,8 +1,8 @@
 const event = data.events
 const currentDate = data.currentDate
 
-for (let x=0;x<event.length;x++) {
-        if (event[x].date >= currentDate) {
-            console.log(event[x].name, event[x].date);            
-        }
+for (let x=0;x<data.events.length;x++) {
+    if (data.events[x].date >= data.currentDate) {
+        console.log(data.events[x].name, data.events[x].date);            
+    }
 }
--- Script/home.js
+++ Script/home.js
@@ -1,6 +1,3 @@
-const event = data.events
-const currentDate = data.currentDate
-
 for (x in data.events) {
         console.table(data.events[x]); 
 }
